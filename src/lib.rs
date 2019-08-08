e3bedd7dd6fd4d37e49c0884cbf4de6d80487ac8
Alberto Piai <apiai@sensational.ch>
2019-08-08 15:37:05 +0200

    fix tests

--- src/lib.rs
+++ src/lib.rs
@@ -23,29 +23,29 @@ use hunspell_sys::*;
 
 #[test]
 fn create_and_destroy() {
-    let hs = Hunspell::new("/usr/share/hunspell/en_US.aff",
-                           "/usr/share/hunspell/en_US.dic");
+    let hs = Hunspell::new("tests/fixtures/reduced.aff",
+                           "tests/fixtures/reduced.dic");
 }
 
 #[test]
 fn check() {
-    let hs = Hunspell::new("/usr/share/hunspell/en_US.aff",
-                           "/usr/share/hunspell/en_US.dic");
-    assert!(hs.check("programming"));
-    assert!(!hs.check("progrmaing"));
+    let hs = Hunspell::new("tests/fixtures/reduced.aff",
+                           "tests/fixtures/reduced.dic");
+    assert!(hs.check("cats"));
+    assert!(!hs.check("nocats"));
 }
 
 #[test]
 fn suggest() {
-    let hs = Hunspell::new("/usr/share/hunspell/en_US.aff",
-                           "/usr/share/hunspell/en_US.dic");
+    let hs = Hunspell::new("tests/fixtures/reduced.aff",
+                           "tests/fixtures/reduced.dic");
     assert!(hs.suggest("progra").len() > 0);
 }
 
 #[test]
 fn stem() {
-    let hs = Hunspell::new("/usr/share/hunspell/en_US.aff",
-                           "/usr/share/hunspell/en_US.dic");
+    let hs = Hunspell::new("tests/fixtures/reduced.aff",
+                           "tests/fixtures/reduced.dic");
     let cat_stem = hs.stem("cats");
     assert!(cat_stem[0] == "cat");
 }
