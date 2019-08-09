4bd5924bae6960b39ae5a61deaddb495962750f4
Alberto Piai <apiai@sensational.ch>
2019-08-09 17:02:45 +0200

    opt into 2018 edition
    Because why not :)

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,6 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-extern crate hunspell_sys;
 
 use std::ffi::{CStr, CString};
 use std::ptr::null_mut;
