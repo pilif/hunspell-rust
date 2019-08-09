02fe8ded181f32b1ff09d663eac12dacbde8eb1c
Alberto Piai <apiai@sensational.ch>
2019-08-09 09:03:04 +0200

    move tests in a submodule

--- src/lib.rs
+++ src/lib.rs
@@ -20,35 +20,6 @@ use std::ptr::null_mut;
 
 use hunspell_sys::*;
 
-#[test]
-fn create_and_destroy() {
-    let hs = Hunspell::new("tests/fixtures/reduced.aff",
-                           "tests/fixtures/reduced.dic");
-}
-
-#[test]
-fn check() {
-    let hs = Hunspell::new("tests/fixtures/reduced.aff",
-                           "tests/fixtures/reduced.dic");
-    assert!(hs.check("cats"));
-    assert!(!hs.check("nocats"));
-}
-
-#[test]
-fn suggest() {
-    let hs = Hunspell::new("tests/fixtures/reduced.aff",
-                           "tests/fixtures/reduced.dic");
-    assert!(hs.suggest("progra").len() > 0);
-}
-
-#[test]
-fn stem() {
-    let hs = Hunspell::new("tests/fixtures/reduced.aff",
-                           "tests/fixtures/reduced.dic");
-    let cat_stem = hs.stem("cats");
-    assert!(cat_stem[0] == "cat");
-}
-
 pub struct Hunspell {
     handle: *mut Hunhandle
 }
@@ -130,5 +101,39 @@ impl Drop for Hunspell {
         unsafe {
             Hunspell_destroy(self.handle);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Hunspell;
+
+    #[test]
+    fn create_and_destroy() {
+        let hs = Hunspell::new("tests/fixtures/reduced.aff",
+                               "tests/fixtures/reduced.dic");
+    }
+
+    #[test]
+    fn check() {
+        let hs = Hunspell::new("tests/fixtures/reduced.aff",
+                               "tests/fixtures/reduced.dic");
+        assert!(hs.check("cats"));
+        assert!(!hs.check("nocats"));
+    }
+
+    #[test]
+    fn suggest() {
+        let hs = Hunspell::new("tests/fixtures/reduced.aff",
+                               "tests/fixtures/reduced.dic");
+        assert!(hs.suggest("progra").len() > 0);
+    }
+
+    #[test]
+    fn stem() {
+        let hs = Hunspell::new("tests/fixtures/reduced.aff",
+                               "tests/fixtures/reduced.dic");
+        let cat_stem = hs.stem("cats");
+        assert!(cat_stem[0] == "cat");
     }
 }
